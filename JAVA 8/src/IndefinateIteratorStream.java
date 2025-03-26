6010c834a5bae1e44b5d209e368fc31f9ec64a80
harshkoshta <koshta15@gmail.com>
2025-03-26 17:19:02 +0000

    added 2 cases to know type in Collection.groupingby lambda function

--- JAVA 8/src/IndefinateIteratorStream.java
+++ JAVA 8/src/IndefinateIteratorStream.java
@@ -1,8 +1,22 @@
+import java.util.List;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class IndefinateIteratorStream {
     public static void main(String[] args) {
         Stream<Integer> infStream = Stream.iterate(2,n-> n+2);
         infStream.forEach(System.out::println);
+
+//        when using groupingBy query
+        // Case 1 here TreeMap::new, here compiler use type inference help function to deteermine the type
+        TreeMap<Integer, List<String>> collect = Stream.of("Hello", "hi", "by", "bye").collect(Collectors.groupingBy(String::length, TreeMap::new, Collectors.toList()));
+
+        //case 2 if type inferrence work then we need to pass the type inside TreeMap
+        TreeMap<Integer, List<String>> collect2 = Stream.of("Hello", "hi", "by", "bye").collect(Collectors.groupingBy(String::length, () -> new TreeMap<Integer, List<String>>(), Collectors.toList()));
+
+
     }
 }
